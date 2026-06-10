b70b9bfdd6db6c7602455181dfddca7aa2da4b21
Andres0607MP <andresmancera06156@gmail.com>
2026-06-10 16:32:54 -0500

    Implementacion de encabezado y pie de pagina

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar.component';
+import { FooterComponent } from './shared/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
